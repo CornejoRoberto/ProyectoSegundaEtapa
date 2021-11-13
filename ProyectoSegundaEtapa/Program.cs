266d468749edafcb36f4b7389090f246f947c78d
CornejoRoberto <roberto_samp@outlook.com>
2021-11-12 22:36:26 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoSegundaEtapa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoSegundaEtapa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
