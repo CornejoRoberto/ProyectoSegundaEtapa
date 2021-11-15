6e194be6389fa7bd6be0753976e806ca9ebad6d0
xELDIEGOx <juandiegoat58@gmail.com>
2021-11-14 21:50:24 -0600

    Cambios en la clase main (Arreglos generales y métodos extra )

--- ProyectoSegundaEtapa/Program.cs
+++ ProyectoSegundaEtapa/Program.cs
@@ -7,139 +7,26 @@ namespace ProyectoSegundaEtapa
     {
         static void Main(string[] args)
         {
-            int num, num2;
-            
-            Console.WriteLine("----BIENVENIDO A SPRITH INDUSTRRY'S----\n");
 
-            Console.WriteLine("Ingrese 1 si es nuevo usuario o ingrese 2 si ya posee una cuenta ");
-            int opciones = int.Parse(Console.ReadLine());
-
-            switch (opciones)
+            string confirmar = "";
+            int comparar = 0;
+            do
             {
-                case 1:
-                    Console.WriteLine("Ingrese los siguientes datos para la creación de su nueva cuenta de usuario: \n");
-                    string User = IngresoNuevoUsuario_User();
-                    string Correo = IngresoNuevoUsuario_Correo();
-                    string Contraseña = IngresoNuevoUsuario_Contraseña();
+                Console.WriteLine("----BIENVENIDO A SPRITH INDUSTRRY'S----\n");
+                        Console.WriteLine("Ingrese los siguientes datos para la creación de su cuenta de usuario: \n");
+                        string User = IngresoNuevoUsuario_User();
+                        string Correo = IngresoNuevoUsuario_Correo();
+                        string Contraseña = IngresoNuevoUsuario_Contraseña();
 
-                    Usuario nuevoUsuario1 = new Usuario(User, Correo, Contraseña);
-                    
-                    Console.WriteLine($"****   Bienvenido {User}   ****");
-                    //Metodo para entrar al programa en si 
-                    break;
-                case 2:
-                    Console.WriteLine("**Ingrese los datos requeridos para continuar**\n");
+                        Usuario nuevoUsuario1 = new Usuario(User, Correo, Contraseña);
+                        Console.WriteLine($"****   Bienvenido {User}   ****");
+                        Inventario();
 
-                    string Username= IngresoUsuario_name();
-                    string Contra = IngresoUsuario_contra();
-                    //nuevoUsuario1.UsuarioNombre(User);
-                    Console.WriteLine();
-                    //
-                    Console.WriteLine("Bienvenido al inventario, seleccione que categoria desea visualizar");
-                    Console.WriteLine("1.Celulares");
-                    Console.WriteLine("2.Camaras");
-                    Console.WriteLine("3.Televisores");
-                    num2 = int.Parse(Console.ReadLine());
-                    if (num2 == 1)
-                    {
-                        //CREACIÓN DE LA LISTA CON LOS MODELOS DE CELULARES Y PRECIOS
-                        Celulares iphone = new Celulares();
-                        iphone.ModeloCelular = "Iphone 6s";
-                        iphone.Precio = 650;
-                        iphone.Cantidad = 30;
-                        Celulares samsung = new Celulares();
-                        samsung.ModeloCelular = "Samsung Galaxy Note";
-                        samsung.Precio = 800;
-                        samsung.Cantidad = 350;
-                        Celulares motorola = new Celulares();
-                        motorola.ModeloCelular = "Motorola G2";
-                        motorola.Precio = 260;
-                        motorola.Cantidad = 20;
-                        Celulares xiaomi = new Celulares();
-                        xiaomi.ModeloCelular = "Xiaomi Note 10";
-                        xiaomi.Precio = 950;
-                        xiaomi.Cantidad = 45;
-                        List<Celulares> ListaCelulares = new List<Celulares>();
-                        ListaCelulares.Add(iphone);
-                        ListaCelulares.Add(samsung);
-                        ListaCelulares.Add(xiaomi);
-                        ListaCelulares.Add(motorola);
-                        //IMPRESION DE LA LISTA DE CELULARES QUE TIENEN HERENCIA DE INVENTARIO
-                        Console.WriteLine("MODELOS DE CELULARES");
-                        foreach (Celulares item in ListaCelulares)
-                        {
-                            Console.WriteLine(item.ModeloCelular + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " telefonos");
-                        }
-                    }
-                    else if (num2 == 2)
-                    {
-                        Camaras nikon = new Camaras();
-                        nikon.ModeloCamara = "Nikon D3500";
-                        nikon.Precio = 840;
-                        nikon.Cantidad = 40;
-                        Camaras canon = new Camaras();
-                        canon.ModeloCamara = "Canon M50";
-                        canon.Precio = 950;
-                        canon.Cantidad = 30;
-                        List<Camaras> ListaCamaras = new List<Camaras>();
-                        ListaCamaras.Add(nikon);
-                        ListaCamaras.Add(canon);
-                        Console.WriteLine("MODELOS DE CAMARAS");
-                        foreach (Camaras item in ListaCamaras)
-                        {
-                            Console.WriteLine(item.ModeloCamara + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " camaras");
-                        }
-                    }
-                    else if (num2 == 3)
-                    {
-                        Televisores lg = new Televisores();
-                        lg.ModeloTelevisores = "LG OLED Z1";
-                        lg.Precio = 965;
-                        lg.Cantidad = 65;
-                        Televisores panasonic = new Televisores();
-                        panasonic.ModeloTelevisores = "Panasonic Tx-58Gx700E";
-                        panasonic.Precio = 890;
-                        panasonic.Cantidad = 80;
-                        Televisores tcl = new Televisores();
-                        tcl.ModeloTelevisores = "TCL 42S6500";
-                        tcl.Precio = 599;
-                        tcl.Cantidad = 78;
-                        List<Televisores> ListaTelevisores = new List<Televisores>();
-                        ListaTelevisores.Add(lg);
-                        ListaTelevisores.Add(panasonic);
-                        ListaTelevisores.Add(tcl);
-                        Console.WriteLine("MODELOS DE TELEVISORES");
-                        foreach (Televisores item in ListaTelevisores)
-                        {
-                            Console.WriteLine(item.ModeloTelevisores + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " televisores");
-                        }
-                    }
-
-                    break;
-                default:
-                    break;
-
-            }
-
-           
-        }
-        public static void General()
-        {
-
-        }
-        public static string IngresoUsuario_name()
-        {
-            string Username = "";
-            Console.WriteLine("**  Ingrese su nombre de usuario  **\n");
-            Username = Console.ReadLine();
-            return Username;
-        }
-        public static string IngresoUsuario_contra()
-        {
-            string contra = "";
-            Console.WriteLine("**  Ingrese su contraseña  **\n");
-            contra = Console.ReadLine();
-            return contra;
+                Console.WriteLine("Saliendo del sistema");
+                Console.WriteLine("¿Deea acceder nuevamente? \nSi/No ");
+                confirmar = Console.ReadLine();
+                comparar = string.Compare(confirmar, "no", true);
+            } while (comparar!=0);
         }
 
         public static string IngresoNuevoUsuario_User()
@@ -147,6 +34,7 @@ namespace ProyectoSegundaEtapa
             String User = "";
             Console.WriteLine("**  Ingrese su nombre de usuario  **\n");
             User = Console.ReadLine();
+            User = VerificarUsuario(User);
             return User;
         }
         public static string IngresoNuevoUsuario_Correo()
@@ -162,7 +50,29 @@ namespace ProyectoSegundaEtapa
             string Contraseña = "";
             Console.WriteLine("**  Ingrese su correo contraseña   **\n");
             Contraseña = Console.ReadLine();
+            Contraseña = VerificarContraseña(Contraseña);
             return Contraseña;
+        }
+
+        private static string VerificarUsuario (string Usuario)
+        {
+            string respuesta = "";
+            int compara = 0;
+            Console.WriteLine("¿Es correcto su nombre de usuario? \nSi/No ");
+            respuesta = Console.ReadLine();
+            compara = string.Compare(respuesta, "si", true);
+
+            if (compara == 0)
+            {
+                return Usuario;
+            }
+            else
+            {
+                Console.WriteLine("--Ingrese nuevamente-- \n");
+                string UsuarioNuevo = Console.ReadLine();
+                Usuario = UsuarioNuevo;
+                return Usuario;
+            }
         }
 
         private static string VerificarCorreo(string Correo)
@@ -185,34 +95,134 @@ namespace ProyectoSegundaEtapa
                 return Correo;
             }
         }
-    }
-}
+        private static string VerificarContraseña(string Contraseña)
+        {
+            string respuesta = "";
+            int compara = 0;
+            Console.WriteLine("¿Es correcto su nombre de usuario? \nSi/No ");
+            respuesta = Console.ReadLine();
+            compara = string.Compare(respuesta, "si", true);
 
-/*
-            Console.WriteLine("Si desea revisar el inventario presione 1, caso contrario presione cualquier otra tecla");
-            num = int.Parse(Console.ReadLine());
-            if (num == 1)
+            if (compara == 0)
             {
-                //CREACIÓN DE LA LISTA CON LOS MODELOS DE CELULARES Y PRECIOS
-                Celulares iphone = new Celulares();
-                iphone.ModeloCelular = "Iphone 6s";
-                iphone.Precio = 650;
-                iphone.Cantidad= 30;
-                Celulares samsung = new Celulares();
-                samsung.ModeloCelular = "Samsung Galaxy Note";
-                samsung.Precio = 800;
-                
-                Celulares xiaomi = new Celulares();
-                xiaomi.ModeloCelular = "Xiaomi Note 10";
-                xiaomi.Precio = 950;
-                List<Celulares> ListaCelulares = new List<Celulares>();
-                ListaCelulares.Add(iphone);
-                ListaCelulares.Add(samsung);
-                ListaCelulares.Add(xiaomi);
-                //IMPRESION DE LA LISTA DE CELULARES QUE TIENEN HERENCIA DE INVENTARIO
-                foreach (Celulares item in ListaCelulares)
+                return Contraseña;
+            }
+            else
+            {
+                Console.WriteLine("--Ingrese nuevamente-- \n");
+                string ContraseñaNueva = Console.ReadLine();
+                Contraseña = ContraseñaNueva;
+                return Contraseña;
+            }
+        }
+
+        public static void Inventario()
+        {
+            int seleccion = 0;
+            do
+            {
+                Console.WriteLine("Ingreso al inventario, seleccione que categoria desea visualizar\n1. Celulare\n2. Camaras\n3. Televisores\n");
+                try
                 {
-                    Console.WriteLine(item.ModeloCelular+"\n"+ "Con un valor de:"+item.Precio+"\n"+ "Hay un catidad de:" +item.Cantidad+" telefonos");
+                    seleccion = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("******Se ingresó un valor incorrecto. Ingrese nuevamente******\n*");
+                    seleccion = 0;
                 }
 
-            }*/
+            } while (seleccion != 1 && seleccion != 2 && seleccion != 3);
+
+            switch (seleccion)
+            {
+                case 1:
+                    //CREACIÓN DE LA LISTA CON LOS MODELOS DE CELULARES Y PRECIOS
+                    Celulares();
+                    break;
+
+                case 2:
+                    //CREACIÓN DE LA LISTA CON LOS MODELOS DE CAMARAS Y PRECIOS
+                    Camaras();
+                    break;
+                case 3:
+                    Televisores();
+                    //CREACIÓN DE LA LISTA CON LOS MODELOS DE TELEVISORES Y PRECIOS
+                    break;
+            }
+        }
+
+
+        public static void Celulares()
+        {
+
+            Celulares iphone = new Celulares();
+            iphone.ModeloCelular = "Iphone 6s";
+            iphone.Precio = 650;
+            iphone.Cantidad = 30;
+            Celulares samsung = new Celulares();
+            samsung.ModeloCelular = "Samsung Galaxy Note";
+            samsung.Precio = 800;
+            samsung.Cantidad = 350;
+            Celulares motorola = new Celulares();
+            motorola.ModeloCelular = "Motorola G2";
+            motorola.Precio = 260;
+            motorola.Cantidad = 20;
+            Celulares xiaomi = new Celulares();
+            xiaomi.ModeloCelular = "Xiaomi Note 10";
+            xiaomi.Precio = 950;
+            xiaomi.Cantidad = 45;
+            List<Celulares> ListaCelulares = new List<Celulares>();
+            ListaCelulares.Add(iphone);
+            ListaCelulares.Add(samsung);
+            ListaCelulares.Add(xiaomi);
+            ListaCelulares.Add(motorola);
+
+            //IMPRESION DE LA LISTA DE CELULARES QUE TIENEN HERENCIA DE INVENTARIO
+            Console.WriteLine("MODELOS DE CELULARES");
+            foreach (Celulares item in ListaCelulares)
+                Console.WriteLine(item.ModeloCelular + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " telefonos");
+        }
+
+        public static void Camaras()
+        {
+            Camaras nikon = new Camaras();
+            nikon.ModeloCamara = "Nikon D3500";
+            nikon.Precio = 840;
+            nikon.Cantidad = 40;
+            Camaras canon = new Camaras();
+            canon.ModeloCamara = "Canon M50";
+            canon.Precio = 950;
+            canon.Cantidad = 30;
+            List<Camaras> ListaCamaras = new List<Camaras>();
+            ListaCamaras.Add(nikon);
+            ListaCamaras.Add(canon);
+            Console.WriteLine("MODELOS DE CAMARAS");
+            foreach (Camaras item in ListaCamaras)
+                Console.WriteLine(item.ModeloCamara + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " camaras");
+        }
+
+        public static void Televisores()
+        {
+            Televisores lg = new Televisores();
+            lg.ModeloTelevisores = "LG OLED Z1";
+            lg.Precio = 965;
+            lg.Cantidad = 65;
+            Televisores panasonic = new Televisores();
+            panasonic.ModeloTelevisores = "Panasonic Tx-58Gx700E";
+            panasonic.Precio = 890;
+            panasonic.Cantidad = 80;
+            Televisores tcl = new Televisores();
+            tcl.ModeloTelevisores = "TCL 42S6500";
+            tcl.Precio = 599;
+            tcl.Cantidad = 78;
+            List<Televisores> ListaTelevisores = new List<Televisores>();
+            ListaTelevisores.Add(lg);
+            ListaTelevisores.Add(panasonic);
+            ListaTelevisores.Add(tcl);
+            Console.WriteLine("MODELOS DE TELEVISORES");
+            foreach (Televisores item in ListaTelevisores)
+                Console.WriteLine(item.ModeloTelevisores + "\n" + "Con un valor de:" + item.Precio + "\n" + "Hay un catidad de:" + item.Cantidad + " televisores");
+        }
+    } 
+}
--- ProyectoSegundaEtapa/Usuario.cs
+++ ProyectoSegundaEtapa/Usuario.cs
@@ -15,49 +15,8 @@ namespace ProyectoSegundaEtapa
             this.Contraseña = Contraseña;
         }
 
-
         public string User { get; set; }
-        public string Email { get; set; }
+        public string Email { get; }
         public string Contraseña { get; set; }
-
-        public void UsuarioContraseña(string Contra)
-        {
-            string RepiteContra = "";
-            if (Contra == Contraseña)
-            {
-                Console.WriteLine("Continue");
-            }
-            else
-            {
-                do
-                {
-                    Console.WriteLine("Ingrese la contraseña nuevamente");
-                    RepiteContra = Console.ReadLine();
-                } while (RepiteContra != Contraseña);
-                Console.WriteLine("Continue");
-            }
-        }
-        public void UsuarioNombre(string Username)
-        {
-            string RepiteUsuario = "";
-            if (Username == User)
-            {
-                Console.WriteLine("Continue");
-            }
-            else
-            {
-                do
-                {
-                    Console.WriteLine("Ingrese el usuario nuevamete");
-                    RepiteUsuario = Console.ReadLine();
-                } while (RepiteUsuario != User);
-                Console.WriteLine("Continue");
-            }
-        }
-
-        public void Muestra()
-        {
-
-        }
     }
 }
